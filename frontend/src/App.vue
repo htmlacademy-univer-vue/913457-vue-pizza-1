d9ff08c81fd6e85c9f2d61ba8985b7b14953f1c0
andrew151612 <belyj@itexp.pro>
2023-09-26 19:22:17 +0300

    feat: подключил основной header и layout к приложению

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,12 @@
 <template>
-  <div class="main__wrapper">
-    <div class="main__header">
-      <img
-        src="@/assets/img/logo.svg"
-        width="300"
-        height="47"
-        alt="V!U!E! Pizza"
-      />
-    </div>
-    <h1>Добро пожаловать!</h1>
-    <p>
-      Это проект V!U!E! Pizza для обучения на профессиональном онлайн‑курсе<br />
-      <b>«Vue.js для опытных разработчиков».</b>
-    </p>
-  </div>
+  <AppHeader />
+  <AppLayout />
 </template>
+
+<script setup>
+import AppHeader from "./layouts/AppHeader.vue";
+import AppLayout from "./layouts/AppLayout.vue";
+</script>
 
 <style lang="scss">
 @import "@/assets/scss/app.scss";
