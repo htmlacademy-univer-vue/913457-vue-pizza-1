7b9c1b0202361ae411a2dfbd121da5ad45be237b
andrew151612 <belyj@itexp.pro>
2023-10-24 20:40:24 +0300

    feat: добавил начальные состояния экземплярам хранилища

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,9 @@
 import { defineStore } from "pinia";
 
 export const useAuthStore = defineStore("auth", {
-  state: () => ({}),
+  state: () => ({
+    userInfo: {},
+  }),
   getters: {},
   actions: {},
 });
